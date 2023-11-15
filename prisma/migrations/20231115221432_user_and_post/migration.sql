b98c40dc979cd4edf5e31611e6158d5d590065e7
Caio Fernando <caiofsr13@gmail.com>
2023-11-15 19:15:40 -0300

    fix typo in post schema

--- prisma/migrations/20231115221432_user_and_post/migration.sql
+++ prisma/migrations/20231115221432_user_and_post/migration.sql
@@ -20,7 +20,7 @@ CREATE TABLE "Post" (
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "is_draft" BOOLEAN NOT NULL DEFAULT false,
-    "restrict" BOOLEAN NOT NULL DEFAULT false,
+    "is_restrict" BOOLEAN NOT NULL DEFAULT false,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "user_id" INTEGER NOT NULL,
