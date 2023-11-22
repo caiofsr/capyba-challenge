3452a7972e62377908cf2d2e28a8b525ff1c9824
Caio Fernando <caiofsr13@gmail.com>
2023-11-22 08:07:28 -0300

    refactor signin tests

--- src/application/use-cases/auth/sign-in.spec.ts
+++ src/application/use-cases/auth/sign-in.spec.ts
@@ -1,40 +1,43 @@
-import { JwtService } from '@nestjs/jwt';
+import { Cache } from 'cache-manager';
+import { TestBed } from '@automock/jest';
 import { SignInUseCase } from './sign-in';
-import { AuthService } from '@infra/auth/auth.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { User } from '@application/entities/user/user';
+import { AuthService } from '@infra/auth/auth.service';
 import { UnauthorizedException } from '@nestjs/common';
 import { makeUser } from '@test/factories/user-factory';
-import { InMemoryUserRepository } from '@test/repositories/in-memory-user-repository';
 
 jest.mock('@nestjs/jwt');
 
-describe('SignIn', () => {
-  let userRepository: InMemoryUserRepository;
-  let jwtService: JwtService;
-  let authService: AuthService;
-  let signIn: SignInUseCase;
+describe.only('SignIn', () => {
   let user: User;
+  let signInUseCase: SignInUseCase;
+  let redisService: jest.Mocked<Cache>;
+  let authService: jest.Mocked<AuthService>;
 
   beforeAll(async () => {
-    userRepository = new InMemoryUserRepository();
-    jwtService = new JwtService();
-    authService = new AuthService(userRepository, jwtService);
-    signIn = new SignInUseCase(authService);
+    const { unit, unitRef } = TestBed.create(SignInUseCase).compile();
+    signInUseCase = unit;
+    authService = unitRef.get(AuthService);
+    redisService = unitRef.get(CACHE_MANAGER);
+
     user = await makeUser({ password: 'super secret password' });
   });
 
   it('should be able to create a new access token', async () => {
-    userRepository.create(user);
+    authService.validateUser.mockResolvedValue(user);
+    authService.login.mockResolvedValue({ accessToken: 'access token' });
 
-    const { type } = await signIn.execute(user.email, 'super secret password');
+    await signInUseCase.execute(user.email, 'super secret password');
 
-    expect(jwtService.sign).toHaveBeenCalled();
-    expect(type).toEqual('Bearer');
+    expect(authService.validateUser).toHaveBeenCalled();
+    expect(redisService.set).toHaveBeenCalled();
+    expect(authService.login).toHaveBeenCalled();
   });
 
   it('should throw if the password is wrong', () => {
     expect(() => {
-      return signIn.execute(user.email, 'this is a wrong password');
-    }).rejects.toThrow(new UnauthorizedException());
+      return signInUseCase.execute(user.email, 'this is a wrong password');
+    }).rejects.toThrow(new UnauthorizedException('Email and Password does not match.'));
   });
 });
