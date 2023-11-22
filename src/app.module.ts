06632f05a05f0cdcc06940ab73ff239424dcff45
Caio Fernando <caiofsr13@gmail.com>
2023-11-22 08:08:56 -0300

    add nodemailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import type { RedisClientOptions } from 'redis';
 import { CacheModule } from '@nestjs/cache-manager';
 import { AuthModule } from '@infra/auth/auth.module';
 import { HttpModule } from '@infra/http/http.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 import * as redisStore from 'cache-manager-redis-store';
 import { UploadModule } from '@infra/upload/upload.module';
 import { DatabaseModule } from '@infra/database/database.module';
@@ -15,6 +16,17 @@ import { DatabaseModule } from '@infra/database/database.module';
       isGlobal: true,
       store: redisStore,
       url: process.env.REDIS_URL,
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        secure: false,
+        port: Number(process.env.SMTP_PORT),
+        ignoreTLS: true,
+      },
+      defaults: {
+        from: process.env.SMTP_FROM,
+      },
     }),
     DatabaseModule,
     AuthModule,
